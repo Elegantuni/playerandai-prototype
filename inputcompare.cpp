1952945b261fb864f92e59c615e1b67ae7bc0806
Anthony Brown <groundup2360917182914017@gmail.com>
2020-04-18 11:42:41 -0400

    Make clang++ work

--- inputcompare.cpp
+++ inputcompare.cpp
@@ -3,14 +3,14 @@
 int inputcompare(void *type, char character)
 {
 #ifdef INITNCURSESNOW
-	if((int)type == character)
+	if((int)(size_t)type == character)
 	{
 		return 1;
 	}
 #endif
 
 #ifdef INITLIBTCODNOW
-	if((char)type == character)
+	if((char)(size_t)type == character)
 	{
 		return 1;
 	}
